46d9cb539eb3ce381d33e4287b677101bd48543f
Niki-q <petrychenko.nikita.learn@gmail.com>
2023-03-22 23:51:59 +0200

    wrapping code in a function, removing unused code

--- app/app.py
+++ app/app.py
@@ -1,49 +1,50 @@
 import os
 import pandas as pd
 import psycopg2
+import warnings
 
-CSV_PATH = {
-    2021: {'path': './data/Odata2021File.csv',
-           'encoding': 'UTF-8-SIG'
-           },
-    2020: {'path': './data/Odata2020File.csv',
-           'encoding': 'windows-1251'
-           },
-    2019: {'path': './data/Odata2019File.csv',
-           'encoding': 'windows-1251'
-           },
-    2018: {'path': './data/OpenData2018.csv',
-           'encoding': 'UTF-8-SIG'
-           },
-    2017: {'path': './data/OpenData2017.csv',
-           'encoding': 'UTF-8-SIG'
-           },
-    2016: {'path': './data/OpenData2016.csv',
-           'encoding': 'windows-1251'
-           }
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+CSV_CONFIG = {
+    'PATH_CONFIG': {
+        2021: {'path': './data/Odata2021File.csv',
+               'encoding': 'UTF-8-SIG'
+               },
+        2020: {'path': './data/Odata2020File.csv',
+               'encoding': 'windows-1251'
+               },
+        2019: {'path': './data/Odata2019File.csv',
+               'encoding': 'windows-1251'
+               },
+        2018: {'path': './data/OpenData2018.csv',
+               'encoding': 'UTF-8-SIG'
+               },
+        2017: {'path': './data/OpenData2017.csv',
+               'encoding': 'UTF-8-SIG'
+               },
+        2016: {'path': './data/OpenData2016.csv',
+               'encoding': 'windows-1251'
+               }
+    },
+    'OPEN_CONFIG': {
+        'error_bad_lines': False,
+        'warn_bad_lines': False,
+        'delimiter': ';',
+        'na_values': ['null'],
+        'engine': 'python'
+    }
 }
-
-CSV_OPTIONS = {
-    'error_bad_lines': False, 'warn_bad_lines': False,
-    # 'skip_bad_lines': True,
-    'delimiter': ';', 'na_values': ['null'], 'engine': 'python'}
-
-# получаем значения для подключения к БД из переменных окружения Docker
-
-db_port = os.environ.get('DB_PORT')
-db_name = os.environ.get('POSTGRES_DB')
-db_user = os.environ.get('POSTGRES_USER')
-db_password = os.environ.get('POSTGRES_PASSWORD')
 
 
 def push_to_sql(tabla_name, dataframe):
     # создаем подключение к БД
     conn = psycopg2.connect(
+        # получаем значения для подключения к БД из переменных окружения Docker
         host='db',
-        port=db_port,
-        dbname=db_name,
-        user=db_user,
-        password=db_password
+        port=os.environ.get('DB_PORT'),
+        dbname=os.environ.get('POSTGRES_DB'),
+        user=os.environ.get('POSTGRES_USER'),
+        password=os.environ.get('POSTGRES_PASSWORD')
     )
 
     # загрузка DataFrame в базу данных PostgreSQL
@@ -54,36 +55,26 @@ def push_to_sql(tabla_name, dataframe):
     conn.close()
 
 
-# определяем функцию для обработки строк, которые не могут быть разобраны
-def handle_bad_lines(line):
-    print(f'Error line - {line}')
-    pass
+def merge_dataframe_from_csv_files(csv_path_config, csv_openfile_options):
+    # Создаем пустой список для хранения данных из каждого файла
+    dataframes = []
+    shape_list = []
+
+    for year in csv_path_config:
+        path = csv_path_config[year]['path']
+        encoding = csv_path_config[year]['encoding']
+        df = pd.read_csv(path, encoding=encoding, **csv_openfile_options)
+        df['YEAR'] = year
+
+        # Добавляем DataFrame в список dataframes
+        dataframes.append(df)
+        shape_list.append(df.shape)
+
+    return pd.concat(dataframes)
 
 
-# Создаем пустой список для хранения данных из каждого файла
-dataframes = []
-shape_list = []
+if __name__ == '__main__':
+    merged_df = merge_dataframe_from_csv_files(CSV_CONFIG['PATH_CONFIG'], CSV_CONFIG['OPEN_CONFIG'])
 
-for year in CSV_PATH:
-    print(year, 'already imported to dataframe')
-    path = CSV_PATH[year]['path']
-    encoding = CSV_PATH[year]['encoding']
-
-    df = pd.read_csv(path,
-                     # on_bad_lines=handle_bad_lines,
-                     encoding=encoding, **CSV_OPTIONS)
-    df['YEAR'] = year
-
-    # Добавляем DataFrame в список dataframes
-    dataframes.append(df)
-    shape_list.append(df.shape)
-
-merged_df = pd.concat(dataframes)
-print(shape_list)
-print(merged_df.shape)
-print(merged_df.columns)
-
-random_sample = merged_df.sample(n=30)
-
-print(random_sample.to_string())
-random_sample.to_csv('random-sample.csv')
+    random_sample = merged_df.sample(n=30)
+    random_sample.to_csv('random-sample.csv')
