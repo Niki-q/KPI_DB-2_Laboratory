97ffa011b73cf55c5005f9137c8d5de317c76437
Niki-q <petrychenko.nikita.learn@gmail.com>
2023-05-19 19:51:03 +0300

    Created docker-architecture for migrate program and fix other docker files.

---
+++ migrate/migrate.py
@@ -0,0 +1,15 @@
+import logging
+
+def init_logging():
+    """
+    Инициализирует журналирование (логирование) для текущего модуля с уровнем DEBUG.
+
+    Возвращает:
+    Экземпляр логгера для текущего модуля.
+    """
+    logging.basicConfig(level=logging.DEBUG)
+    return logging.getLogger(__name__)
+
+
+logger = init_logging()
+logger.info('Test')
