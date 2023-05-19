b6367dbf3a5b15a95d2b626079b46eae02603301
Niki-q <petrychenko.nikita.learn@gmail.com>
2023-05-19 19:58:24 +0300

    Create empty flask-migrate structure.

--- migrate/migrate.py
+++ migrate/migrate.py
@@ -1,4 +1,16 @@
 import logging
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+DB_CONFIG = {'user': os.getenv("DB_USER"),
+             'password': os.getenv("DB_PASSWORD"),
+             'host': os.getenv("DB_HOST"),
+             'port': os.getenv("DB_PORT"),
+             'database': os.getenv("DB_NAME")}
+
 
 def init_logging():
     """
@@ -11,5 +23,24 @@ def init_logging():
     return logging.getLogger(__name__)
 
 
-logger = init_logging()
-logger.info('Test')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://" \
+                                        f"{DB_CONFIG['user']}" \
+                                        f":{DB_CONFIG['password']}" \
+                                        f"@{DB_CONFIG['host']}" \
+                                        f":{DB_CONFIG['port']}" \
+                                        f"/{DB_CONFIG['database']}"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
+
+if __name__ == '__main__':
+    logger = init_logging()
+    logger.info('Test')
+
+    app.run()
