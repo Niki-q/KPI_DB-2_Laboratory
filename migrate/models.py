62e89cfe9a8f717ba3aced2fed4f82d932d75124
Niki-q <petrychenko.nikita.learn@gmail.com>
2023-05-30 14:19:48 +0300

    models change

--- migrate/models.py
+++ migrate/models.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 class EducationalInstitution(db.Model):
     __tablename__ = 'educational_institutions'
 
-    ID = db.Column(db.String, primary_key=True)
+    ID = db.Column(db.Integer, primary_key=True,  autoincrement=True)
     TypeName = db.Column(db.String)
     AreaName = db.Column(db.String)
     TerName = db.Column(db.String)
@@ -21,28 +21,28 @@ class EducationalInstitution(db.Model):
 class Participant(db.Model):
     __tablename__ = 'participants'
 
-    ID = db.Column(db.String, primary_key=True)
+    ID = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
     Birth = db.Column(db.Integer)
     SexTypeName = db.Column(db.String)
     RegName = db.Column(db.String)
     AreaName = db.Column(db.String)
-    Ter_id = db.Column(db.String, db.ForeignKey('territories.ID'))
+    Ter_id = db.Column(db.Integer, db.ForeignKey('territories.ID'))
     ClassProfileName = db.Column(db.String)
     ClassLangName = db.Column(db.String)
-    EO_id = db.Column(db.String, db.ForeignKey('educational_institutions.ID'))
+    EO_id = db.Column(db.Integer, db.ForeignKey('educational_institutions.ID'))
 
 
 class Territory(db.Model):
     __tablename__ = 'territories'
 
-    ID = db.Column(db.String, primary_key=True)
+    ID = db.Column(db.Integer, primary_key=True,  autoincrement=True)
     Name = db.Column(db.String)
     TypeName = db.Column(db.String)
 
 
 class PointOfObservation(db.Model):
     __tablename__ = 'points_of_observation'
-    ID = db.Column(db.String, primary_key=True)
+    ID = db.Column(db.Integer, primary_key=True,  autoincrement=True)
     Name = db.Column(db.String)
     RegName = db.Column(db.String)
     AreaName = db.Column(db.String)
@@ -51,9 +51,9 @@ class PointOfObservation(db.Model):
 
 class Testing(db.Model):
     __tablename__ = 'testings'
-    ID = db.Column(db.String, primary_key=True)
-    Part_ID = db.Column(db.String, db.ForeignKey('participants.ID'))
-    Point_ID = db.Column(db.String, db.ForeignKey('points_of_observation.ID'))
+    ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Part_ID = db.Column(db.String(36), db.ForeignKey('participants.ID'))
+    Point_ID = db.Column(db.Integer, db.ForeignKey('points_of_observation.ID'))
     Year = db.Column(db.Integer)
     Test = db.Column(db.String)
     Lang = db.Column(db.String)
